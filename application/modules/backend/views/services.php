66e21d96e425c9bf34d2b1f55873cea27f2f844e
Sachin Jagtap <sachinj@rebelute.com>
2017-06-16 13:28:06 +0530

    Service page added

--- application/modules/backend/views/services.php
+++ application/modules/backend/views/services.php
@@ -1,1 +1,79 @@
-<?php echo "test";?>
+<!-- page content -->
+<div class="right_col" role="main">
+	<div class="row dashboardServicesWrap">
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading siloSDImgBox">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/silo%20main%20logo.png" alt="silo" width="100">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading websuitImgBox">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/wbssuite.png" alt="wbssuite" width="177" height="61">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading passportImgBox">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/paasport.png" alt="paasport" height="39" width="136">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+		
+	
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/fiberrails%20main96x86.png" alt="fiberrails">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading scandiscImgBox">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/scandisc%20main%20logo.png" alt="scandisc" width="158">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<img class="img-responsive center-block" src="<?php echo base_url(); ?>images/services/silobank.png" alt="silobank" width="109">
+				</div>
+				<div class="panel-body">
+					<h4 class="heading">Work Brakdown Structure Suite</h4>
+					<p class="description">Not so bold details about the products</p>
+					<a href="" class="btn btnRed">Read More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
